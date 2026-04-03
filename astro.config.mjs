0763854d69e519f24c1d6f3f121201c4734fefcb
Jeremy Thierry Chan <101402375+JeremyThierryChan@users.noreply.github.com>
2026-04-03 19:31:44 +0800

    Changed to Vite

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  site: 'https://jeremythierrychan.github.io',
-  base: '/InChief/',
-  output: 'static',
-  integrations: [tailwind()]
+  site: "https://jeremythierrychan.github.io",
+  base: "/InChief/",
+  output: "static",
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
