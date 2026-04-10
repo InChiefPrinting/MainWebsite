02c7df4139a13585664fb0b513e540e86a886f43
Jeremy Thierry Chan <101402375+JeremyThierryChan@users.noreply.github.com>
2026-04-10 08:59:42 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: "https://inchiefprinting.github.io",
-  base: "/MainWebsite",
+  base: "/MainWebsite/",
   output: "static",
   vite: {
     plugins: [tailwindcss()],  
