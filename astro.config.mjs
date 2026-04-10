ec1478a2e6fc3d90d9384d84ec8b655095dabbe9
Jeremy Thierry Chan <101402375+JeremyThierryChan@users.noreply.github.com>
2026-04-10 08:45:08 +0800

    1

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: "https://jeremythierrychan.github.io",
-  base: "/InChief/",
+  base: "/",
   output: "static",
   vite: {
     plugins: [tailwindcss()],  
